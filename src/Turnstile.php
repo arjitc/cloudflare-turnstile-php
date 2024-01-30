0b006c26ee1a6d0b4036a7753277a999414cd0bb
Arjit Chaudhary <arjit.c@gmail.com>
2024-01-31 02:22:04 +0530

    Create Turnstile.php

---
+++ src/Turnstile.php
@@ -0,0 +1,42 @@
+<?php
+class Turnstile {
+
+    private $post;
+    private $secret;
+
+    function __construct($secret) {
+        $this->post   = $_POST;
+        $this->secret = $secret;
+    }
+
+    public function verify() {
+        // set post fields
+        $post = [
+            'response' => $this->post['cf-turnstile-response'],
+            'secret'   => $this->secret,
+        ];
+
+        $ch = curl_init("https://challenges.cloudflare.com/turnstile/v0/siteverify");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 3); // timeout in 5 seconds
+
+        // execute!
+        $response = curl_exec($ch);
+
+        // close the connection, release resources used
+        curl_close($ch);
+
+        // do anything you want with your response
+        $response = json_decode($response, true);
+
+        if ($response['success'] == true) {
+            return true;
+        } elseif ($response['success'] == false) {
+            return $response['error-codes']['0'];
+        } else {
+            return false;
+        }
+    }
+}
